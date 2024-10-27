45d52be277d3b86732d24a5608a569a378f32083
Ersan Karimi <ersankarimii311@gmail.com>
2024-10-27 17:23:58 +0800

    refactor: Update tailwind.config.ts to use ploraPreset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "tailwindcss";
-import preset from "./utils/preset";
+import { ploraPreset } from "./utils/preset";
 
 /** @type {import('tailwindcss').Config} */
 export default <Partial<Config>>{
-  presets: [
-    preset,
-  ],
+  presets: [ploraPreset],
 };
